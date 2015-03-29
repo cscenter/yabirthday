c01f710a73f9f8f758d0ed1809d949a999a76c5a
telezhnaya <olya@MacBook-Air-Olya.local>
2015-03-29 23:47:39 +0300

    delete bad code

--- hw2-cxf/src/main/java/com/example/rs/JaxRsApiApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.rs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api") //change
-public class JaxRsApiApplication extends Application {
-}
