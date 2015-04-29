ae6e224b2393a04a07508c4348e501cef73518de
telezhnaya <olya@MacBook-Air-Olya.local>
2015-04-29 15:00:07 +0300

    now it gives my html

--- hw1/src/main/java/Main.java
+++ hw1/src/main/java/Main.java
@@ -1,14 +1,15 @@
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
-import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception
     {
-        Connection connection = null;
+      /*  Connection connection = null;
         //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
         String url = "jdbc:postgresql://127.0.0.1:5432/birthday_test1";
         //Имя пользователя БД
@@ -21,7 +22,7 @@ public class Main {
         connection = DriverManager.getConnection(url, name, password);
 
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                ResultSet.CONCUR_UPDATABLE);
+                ResultSet.CONCUR_UPDATABLE); */
 
         Server server = new Server(8081);
         /*
@@ -29,15 +30,25 @@ public class Main {
         // MyServlet servlet = new MyServlet();
         server.setHandler(handler); */
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
 
-        context.addServlet(new ServletHolder(new MyServlet("It works!")),"/*");
+        resource_handler.setResourceBase("/Users/olya/Documents/yabirthday/hw5-static-html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        server.setHandler(handlers);
+
+       /* ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context); */
+
+        //context.addServlet(new ServletHolder(new MyServlet("It works!")),"/*");
 
         server.start();
         server.join();
        // handler.close();
-        connection.close();
+       // connection.close();
     }
 }
