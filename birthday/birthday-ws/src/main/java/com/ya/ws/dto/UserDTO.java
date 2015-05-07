7c0398b425bf754839e07c2b4832ab8f5c9870f6
telezhnaya <olya@MacBook-Air-Olya.local>
2015-05-07 14:02:53 +0300

    Main controller added, dont know why

--- birthday/birthday-ws/src/main/java/com/ya/ws/dto/UserDTO.java
+++ birthday/birthday-ws/src/main/java/com/ya/ws/dto/UserDTO.java
@@ -1,24 +1,30 @@
 package com.ya.ws.dto;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+
 
 /**
  * Created by olya on 15.04.15.
  */
 public class UserDTO {
     private final String login;
-    private final Calendar birthday; //тип вроде надо заменить
+    private final LocalDate birthday;
 
-    public UserDTO(String login, Calendar birthday) {
+    public UserDTO(String login, LocalDate birthday) {
         this.login = login;
         this.birthday = birthday;
+    }
+
+    protected UserDTO() {
+        birthday = null;
+        login = null;
     }
 
     public String getLogin() {
         return login;
     }
 
-    public Calendar getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 }
---
+++ birthday/birthday-ws/src/main/java/com/ya/ws/service/UserApiService.java
@@ -0,0 +1,95 @@
+package com.ya.ws.service;
+
+import com.ya.domain.UserService;
+import com.ya.domain.model.User;
+import com.ya.ws.dto.UserDTO;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+/**
+ * Created by olya on 07.05.15.
+ */
+@Service
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserApiService {
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @Path("/")
+    public List<UserDTO> list() {
+        return userService.list()
+                .stream()
+                .map(this::convert)
+                .collect(Collectors.toList());
+    }
+
+    @GET
+    @Path("{login}")
+    public UserDTO get(@PathParam("login") String login) {
+        if (login == null) {
+            throw new BadRequestException("Login is null");
+        }
+        try {
+            return convert(userService.get(login));
+        } catch (NoSuchElementException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    @POST
+    public UserDTO create(UserDTO user) {
+        if (user == null) {
+            throw new BadRequestException("User is null");
+        } else if (user.getLogin() == null) {
+            throw new BadRequestException("User login is null");
+        } else if (user.getBirthday() == null) {
+            throw new BadRequestException("User birthday is null");
+        }
+        return convert(userService.create(user.getLogin(), user.getBirthday()));
+    }
+
+    @PUT
+    @Path("{login}")
+    public UserDTO update(@PathParam("login") String login, UserDTO user) {
+        if (login == null) {
+            throw new BadRequestException("Login is null");
+        } else if (user == null) {
+            throw new BadRequestException("User is null");
+        } else if (user.getBirthday() == null) {
+            throw new BadRequestException("User birthday is null");
+        }
+        try {
+            return convert(userService.update(login, user.getBirthday()));
+        } catch (NoSuchElementException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    @DELETE
+    @Path("{login}")
+    public UserDTO remove(@PathParam("login") String login) {
+        if (login == null) {
+            throw new BadRequestException("Login is null");
+        }
+        try {
+            return convert(userService.remove(login));
+        } catch (NoSuchElementException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    private UserDTO convert(User user) {
+        return new UserDTO(user.getLogin(), user.getBirthday());
+    }
+}
+
